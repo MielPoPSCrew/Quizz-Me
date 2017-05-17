146938e264fe0681e6b38fa46712ecaa185c8e2b
CottinThomas <thomas_cottin@yahoo.fr>
2017-05-17 20:11:50 +0200

    End put frame in place

--- app.js
+++ app.js
@@ -1,11 +1,35 @@
+// set execution mode
+const MODE = "debug";
+
+// base modules to import
 const express = require('express');
 const fs = require('fs');
+const bodyparser = require('body-parser');
+const nunjucks = require('nunjucks');
 
-var app = express();
+let app = express();
 
+// setup MongoDB connection
 global.DB = require('monk')('localhost/questions_game');
 
-DB.get('questions').insert({'test':'test'});
+// import test scripts if we're in debug
+if(MODE == 'debug'){
+    let rock_script = JSON.parse(fs.readFileSync('samples/rock_sample.json','utf-8'));
+    let rock_script_name = rock_script.name;
 
+    DB.get('questions').find({name: rock_script_name}).then((script) => {
+        if(script.length === 0){
+            DB.get('questions').insert(rock_script);
+        }
+    }).catch(console.error);
+}
+
+nunjucks.configure('views', {express: app});
+app.set('view engine', 'njk');
+app.use(bodyparser.urlencoded({
+    extended: false
+}));
+app.use(express.static('public'));
+app.use(require('./routes/'));
 
 app.listen(8737);
---
+++ routes/games/index.js
@@ -0,0 +1,24 @@
+const router = require('express').Router();
+
+
+/*
+ * Get the list of available games
+ */
+router.get('/', function(req, res, next) {
+    DB.get('questions').find().then((games) => {
+       let pendingGames = [];
+       let freeGames = [];
+       games.forEach((game) => {
+          if(game.nbPlayer !== 0)
+          {
+              pendingGames.push(game);
+          }
+          else{
+              freeGames.push(game);
+          }
+          res.render('games', {pendingGames,freeGames});
+       });
+    });
+});
+
+module.exports = router;
