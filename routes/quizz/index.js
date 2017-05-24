4cd6a5873a7f738f478cd52e1053436938da771f
CottinThomas <thomas_cottin@yahoo.fr>
2017-05-24 13:57:51 +0200

    Quiz list front/back integration

--- routes/quizz/index.js
+++ routes/quizz/index.js
@@ -1,7 +1,12 @@
 const router = require('express').Router();
 
-router.get('/', function(req, res, next) {
-    res.render('quizz/list');
+router.get('/', function(req, res) {
+    DB.get('quiz').find({}).then( (quiz) => {
+        DB.get('topics').find({}).then( (topics) =>{
+            topics.unshift({_id:-1, name: "Tous"});
+            res.render('quizz/list', {quiz, topics});
+        })
+    });
 });
 
 router.get('/create', function(req, res, next) {
