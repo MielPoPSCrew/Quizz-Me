10dec0414485ec535ac2b10ac76bbf2f21419b1f
Romain Laneuville <romain.laneuville@hotmail.fr>
2017-05-25 05:41:55 +0200

    [Back] ~ socket clean es6 class

---
+++ sockets/Game.js
@@ -0,0 +1,116 @@
+import _ from 'lodash-es';
+
+/**
+ * @typedef {Object} Game
+ * @property {string} _id - The game ID
+ * @property {string} name - The game name
+ * @property {UserMongoLike} creator - The game creator
+ * @property {number} created - The game creation timestamp
+ * @property {QuizMongoLike} quiz - The game quiz
+ * @property {UserMongoLike[]} users - The game users
+ * @property {boolean} opened - If the game is opened
+ * @property {boolean} private - If the game is private
+ */
+
+/**
+ * @typedef {Object} GameMongoLike
+ * @property {string} _id - The game ID
+ * @property {string} name - The game name
+ * @property {string} creator - The game creator ID
+ * @property {number} created - The game creation timestamp
+ * @property {string} quiz - The game quiz ID
+ * @property {string[]} users - The game users ID
+ * @property {boolean} opened - If the game is opened
+ * @property {boolean} private - If the game is private
+ */
+
+/**
+ * @typedef {Object} QuizMongoLike
+ * @property {string} _id - The quiz ID
+ * @property {string} name - The quiz name
+ * @property {string} creator - The quiz creator ID
+ * @property {number} created - The quiz creation timestamp
+ * @property {QuizMongoLikeQuestion[]} questions - The quiz questions
+ */
+
+/**
+ * @typedef {Object} QuizMongoLikeQuestion
+ * @property {string} question - The question name
+ * @property {string[]} choices - The quiz answer choice
+ * @property {number} answer - The quiz answer key in choices array
+ * @property {string} info - Additional information about the question
+ */
+
+/**
+ * @typedef {Object} UserMongoLike
+ * @property {string} _id - The user ID
+ * @property {string} username - The user name
+ * @property {number} created - The user creation timestamp
+ * @property {UserMongoLikeStatistics} statistics - The user statistics
+ */
+
+/**
+ * @typedef {Object} UserMongoLikeStatistics
+ * @property {number} gamesTotal - The total number of games played
+ * @property {number} gamesWon - The total number of games won
+ * @property {number} goodAnswers - The total number of good answers
+ * @property {number} quickAnswers - The total number of quick answers
+ */
+
+/**
+ * Game object containing all the game information
+ */
+export default class Game {
+    /**
+     * Constructor
+     */
+    constructor () {
+        this.name    = '';
+        this.creator = {};
+        this.created = 0;
+        this.quiz    = {};
+        this.users   = [];
+        this.opened  = false;
+        this.private = false;
+    }
+
+    /**
+     * Load game information from mongo like objects
+     *
+     * @param {GameMongoLike} gameMongoLike - Game mongo like object
+     * @param {QuizMongoLike} quizMongoLike - Quiz mongo like object
+     * @param {UserMongoLike[]} usersMongoLike - Users mongo like object
+     *
+     * @return {undefined}
+     */
+    loadGameFromMongo (gameMongoLike, quizMongoLike, usersMongoLike) {
+        const self = this;
+
+        this.name         = gameMongoLike.name;
+        this.creator      = _.find(usersMongoLike, {"_id": gameMongoLike.creator});
+        this.created      = gameMongoLike.created;
+        this.quiz         = quizMongoLike;
+        this.quiz.creator = _.find(usersMongoLike, {"_id": quizMongoLike.creator});
+        this.users        = [];
+        this.opened       = gameMongoLike.opened;
+        this.private      = gameMongoLike.private;
+
+        _.forEach(gameMongoLike.users, (id) => {
+            const user = _.find(usersMongoLike, {"_id": id});
+
+            if (_.isUndefined(user)) {
+                throw new Error(`The user with ID = ${id} was not on the users mongo array.`);
+            }
+
+            self.users.push(user);
+        });
+
+        if (_.isUndefined(this.creator)) {
+            throw new Error('Game creator was not on the users mongo array.');
+        }
+
+        if (_.isUndefined(this.quiz.creator)) {
+            throw new Error('Quiz creator was not on the users mongo array.');
+        }
+    }
+}
--- sockets/games/join.js
+++ sockets/games/join.js
@@ -1,147 +1,31 @@
-const _ = require('lodash');
-
-// @todo timeout
-// @todo reset answers and timer
-// @todo refacto with functions
+import GameManagement from '../GameManagement';
 
 module.exports = (io) => {
-    // Game info
-    let currentRound = 0,
-        answered     = 0,
-        timeout      = 10000, // 10 sec
-        timer        = new Date(),
-        scores       = {},
-        dataGame     = {
-            "game": null,
-            "users": [],
-            "rounds": []
-        };
+    const gameManagement = new GameManagement();
 
-    // On connection event
+    // On user connect
     io.on('connection', (socket) => {
-        "use strict";
-
-        let gameId   = socket.handshake.query.gameid,
-            username = '';
-
-        // Check room ID
-        if (_.isUndefined(gameId)) {
-            // TODO : gérer les erreurs
-            console.log('No game ID in parameter');
+        try {
+            gameManagement.userConnect(socket, socket.handshake.username, socket.handshake.gameId);
+        } catch (e) {
+            socket.emit("error", e);
         }
-
-        // Get the username from cookie
-        _.forEach(_.split(socket.handshake.headers.cookie, ';'), (v) => {
-            let tmp = _.split(v, '=');
-
-            if (tmp[0] == 'username') {
-                username = tmp[1];
-            }
-        });
-
-        if (username === '') {
-            console.log('username not found in cookie');
-        }
-
-        // Get game info
-        DB.get('games').findOne({ "_id": gameId}).then((game) => {
-            if (!(game)) {
-                console.log('game error');
-            } else {
-                // Add the game info to the game session
-                dataGame.game = game;
-
-                // Get user info
-                DB.get('users').findOne({ "username" : username}).then((user) => {
-                    if (!(user)) {
-                        console.log('user error');
-                    } else {
-                        if (!_.isUndefined(_.find(dataGame.users, user))) {
-                            console.log('ERROR: user already in the game');
-                        }
-
-                        // Add the user to the game session
-                        dataGame.users.push(user);
-
-                        // Get answers
-                        DB.get('quizz').findOne({ "_id" : dataGame.game.quiz}).then((quiz) => {
-                            dataGame.game.quiz = quiz;
-
-                            // Send the event to all the users in the room
-                            socket.broadcast.emit("userEnterInTheGame", {"users": dataGame.users});
-
-                            // Send the game info to the user
-                            socket.emit("gameEnter", {
-                                "gameId"           : gameId,
-                                "numberOfQuestions": _.size(dataGame.game.quiz.questions),
-                                "gameTitle"        : dataGame.game.name
-                            });
-                        });
-                    }
-                });
-            }
-        });
     });
-
     // On admin launch game
     io.on('launchGame', (socket) => {
-        "use strict";
-
-        let username = socket.handshake.username;
-
-        if (username !== _.find(dataGame.users._id, dataGame.game.creator).username) {
-            console.log('Your are not the admin of the game');
-            // @todo throw exception
+        try {
+            gameManagement.launchGame(socket, socket.handshake.username);
+        } catch (e) {
+            socket.emit("error", e);
         }
-
-        // Alert users that the game is starting
-        socket.broadcast.emit("gameStart");
-
-        // Send the first question to all users after 5 sec
-        setTimeout(() => {
-            socket.broadcast.emit("roundStart", {
-                "roundNumber": currentRound,
-                "question"   : dataGame.game.quiz.questions[currentRound],
-                "choices"    : dataGame.game.quiz.questions[currentRound].choices
-            });
-        }, 5000);
     });
-
     // On user send answer
     io.on('sendAnswser', (socket) => {
-        "use strict";
-
-        const answer = {
-            "username": socket.handshake.username,
-            "answer"  : _.parseInt(socket.handshake.answer),
-            "time"    : new Date() - timer
-        };
-
-        dataGame.rounds[currentRound].push(answer);
-        scores[socket.handshake.username] += _.size(dataGame.game.quiz.questions) - answered++;
-
-        if (answered === dataGame.users.length) {
-            // @todo update scores tableau d'objets {username, score}
-            socket.broadcast.emit("roundEnd", {
-                "scores"    : scores,
-                "goodAnswer": dataGame.game.quiz.questions[currentRound].answer,
-                "answerInfo": dataGame.game.quiz.questions[currentRound].info
-            });
-            // Alert users that the game is starting
-            socket.broadcast.emit("roundStart", {
-                "roundNumber": ++currentRound,
-                "question"   : dataGame.game.quiz.questions[currentRound].question,
-                "choices"    : dataGame.game.quiz.questions[currentRound].choices
-            });
-            // End of the game if this was the last question
-            if (currentRound === _.size(dataGame.game.quiz.questions)) {
-                socket.emit("gameEnd");
-            }
+        try {
+            gameManagement.recieveAnswer(socket, socket.handshake.username, socket.handshake.answer);
+        } catch (e) {
+            socket.emit("error", e);
         }
-
-        // Alert users that the user answer the question
-        socket.broadcast.emit("userAnswer", {"username": socket.handshake.username});
     });
-
     // @todo disconnect ?
-}
+};
